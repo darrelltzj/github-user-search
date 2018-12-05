eefd2cb09a3b26c39bdd4c03b868d38e547ce32c
Darrell Teo <darrelltzj@gmail.com>
2018-12-05 14:52:54 +0800

    heroku webpack port issue

--- webpack.base.js
+++ webpack.base.js
@@ -1,7 +1,9 @@
+// const path = require('path');
 const webpack = require('webpack');
+// const Dotenv = require('dotenv-webpack');
 require('dotenv').config();
 
-console.log('PORT', process.env.PORT)
+console.log('PORT', process.env.PORT, process.env.RAZZLE_PORT)
 
 console.log('REACT_APP_THEME', process.env.REACT_APP_THEME)
 
@@ -18,9 +20,14 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        PORT: JSON.stringify(process.env.PORT),
+        PORT: JSON.stringify(process.env.RAZZLE_PORT || process.env.PORT),
         REACT_APP_THEME: JSON.stringify(process.env.REACT_APP_THEME),
       },
     }),
+    // new Dotenv({
+    //   path: path.resolve(__dirname, './.env'),
+    //   safe: false,
+    //   silent: false,
+    // }),
   ],
 };
